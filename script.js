4cf6dd2d6450401071ee3c0805a49fc541a8b05e
gdemare <44768462+gdemare@users.noreply.github.com>
2019-11-07 16:52:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -16,45 +16,45 @@ content = [
 ['programming',null,'java',1,0,0,0],
 ['programming',null,'python',1,0,0,0],
 ['projects',null,'raspberry-wifi',1,0,0,0],
-['statistics','data-mining','ACM',1,1,1,0],
-['statistics','data-mining','AFC',1,1,1,0],
-['statistics','data-mining','ANOVA',1,1,1,0],
-['statistics','data-mining','ARMA',1,0,0,0],
-['statistics','data-mining','CAH',1,1,1,0],
-['statistics','data-mining','CDH',1,0,0,0],
-['statistics','data-mining','SVM',1,1,0,0],
-['statistics','data-mining','analyse-discriminante',1,1,1,0],
-['statistics','data-mining','arbre-de-decision',1,1,1,0],
-['statistics','data-mining','boosting',1,0,0,0],
-['statistics','data-mining','carte-auto-adaptative',1,0,0,0],
-['statistics','data-mining','centre-mobiles',1,0,1,0],
-['statistics','data-mining','centroide',1,0,1,0],
-['statistics','data-mining','classificateur-baysien',1,1,0,0],
-['statistics','data-mining','cluestering-variables',1,0,1,0],
-['statistics','data-mining','esperance-maximisation',1,0,1,0],
-['statistics','data-mining','foret-aleatoire',1,1,0,0],
-['statistics','data-mining','lissage-exponentiel',1,0,0,0],
-['statistics','data-mining','perceptron',1,0,0,0],
-['statistics','data-mining','predire-evaluer',0,1,0,0],
-['statistics','data-mining','probleme-correlation',1,0,0,0],
-['statistics','data-mining','regression-lineaire',1,0,1,0],
-['statistics','data-mining','regression-logistique',1,1,1,0],
-['statistics','data-mining','reseaux-neurones',1,1,0,0],
-['statistics','data-mining','serie-temporelle-simple',0,1,0,0],
-['statistics','data-mining','serie-temporelle',1,0,0,0],
-['statistics','methode','cluestering-sans-cible',1,0,0,0],
-['statistics',null,'automatiser',0,1,1,0],
-['statistics',null,'carte',0,1,0,0],
-['statistics',null,'classification',1,0,0,0],
-['statistics',null,'deploy-Rshinny-shinyapps',1,0,0,0],
-['statistics',null,'dictionnaire-statistique',1,0,0,0],
-['statistics',null,'formater-donnees',0,1,1,0],
-['statistics',null,'graphiques',0,1,1,0],
-['statistics',null,'manipuler-donnees',0,1,1,0],
-['statistics',null,'reduction-dimensions',1,0,0,0],
-['statistics',null,'statistique-descriptive',0,1,1,0],
-['statistics',null,'tableau-de-bord',1,0,0,0],
-['statistics',null,'tests',0,1,1,0],
+['statistique','data-mining','ACM',1,1,1,0],
+['statistique','data-mining','AFC',1,1,1,0],
+['statistique','data-mining','ANOVA',1,1,1,0],
+['statistique','data-mining','ARMA',1,0,0,0],
+['statistique','data-mining','CAH',1,1,1,0],
+['statistique','data-mining','CDH',1,0,0,0],
+['statistique','data-mining','SVM',1,1,0,0],
+['statistique','data-mining','analyse-discriminante',1,1,1,0],
+['statistique','data-mining','arbre-de-decision',1,1,1,0],
+['statistique','data-mining','boosting',1,0,0,0],
+['statistique','data-mining','carte-auto-adaptative',1,0,0,0],
+['statistique','data-mining','centre-mobiles',1,0,1,0],
+['statistique','data-mining','centroide',1,0,1,0],
+['statistique','data-mining','classificateur-baysien',1,1,0,0],
+['statistique','data-mining','cluestering-variables',1,0,1,0],
+['statistique','data-mining','esperance-maximisation',1,0,1,0],
+['statistique','data-mining','foret-aleatoire',1,1,0,0],
+['statistique','data-mining','lissage-exponentiel',1,0,0,0],
+['statistique','data-mining','perceptron',1,0,0,0],
+['statistique','data-mining','predire-evaluer',0,1,0,0],
+['statistique','data-mining','probleme-correlation',1,0,0,0],
+['statistique','data-mining','regression-lineaire',1,0,1,0],
+['statistique','data-mining','regression-logistique',1,1,1,0],
+['statistique','data-mining','reseaux-neurones',1,1,0,0],
+['statistique','data-mining','serie-temporelle-simple',0,1,0,0],
+['statistique','data-mining','serie-temporelle',1,0,0,0],
+['statistique','methode','cluestering-sans-cible',1,0,0,0],
+['statistique',null,'automatiser',0,1,1,0],
+['statistique',null,'carte',0,1,0,0],
+['statistique',null,'classification',1,0,0,0],
+['statistique',null,'deploy-Rshinny-shinyapps',1,0,0,0],
+['statistique',null,'dictionnaire-statistique',1,0,0,0],
+['statistique',null,'formater-donnees',0,1,1,0],
+['statistique',null,'graphiques',0,1,1,0],
+['statistique',null,'manipuler-donnees',0,1,1,0],
+['statistique',null,'reduction-dimensions',1,0,0,0],
+['statistique',null,'statistique-descriptive',0,1,1,0],
+['statistique',null,'tableau-de-bord',1,0,0,0],
+['statistique',null,'tests',0,1,1,0],
 ['unix',null,'bash',1,0,0,0],
 ['unix',null,'folders-linux',1,0,0,0],
 ['unix',null,'shell',1,0,0,0],
@@ -62,7 +62,7 @@ content = [
 ['web',null,'css',1,0,0,0],
 ['web',null,'html',1,0,0,0],
 ['web',null,'js',1,0,0,0],
-['web',null,'php',1,0,0,0]
+['web',null,'php',1,0,0,0],
 ];
 
 // transposer le tableau
@@ -126,6 +126,7 @@ function dark() {
     root.style.setProperty('--color-write','rgb(236,238,240)');
     root.style.setProperty('--color-bouton','rgb(44,44,46)');
     root.style.setProperty('--color-nav','rgb(55,54,63)');
+    root.style.setProperty('--color-boite','rgb(33,32,42)');
     root.style.setProperty('--color-fond','rgb(33,32,42)');
     root.style.setProperty('--color-titre','rgb(99,99,102)');
     root.style.setProperty('--color-degrade1','rgb(0,122,255)');
@@ -146,7 +147,7 @@ function dark() {
     root.style.setProperty('--color-boiteH2','rgb(50,50,54)');
     root.style.setProperty('--color-write','rgb(118,118,119)');
     root.style.setProperty('--color-bouton','rgb(232,232,232)');
-    root.style.setProperty('--color-nav','rgb(247,247,250)');
+    root.style.setProperty('--color-nav','rgb(255,255,255)');
     root.style.setProperty('--color-boite','rgb(255,255,255)');
     root.style.setProperty('--color-fond','rgb(247,247,250)');
     root.style.setProperty('--color-titre','rgb(180,185,190)');
@@ -203,9 +204,9 @@ for (var i = 0; i <= content[0].length - 1; i++) {
   };
 
   var langage = "";
-  if (content[4][i] == 1) {langage = langage + '<span class="r">R</span>';};
-  if (content[5][i] == 1) {langage = langage + '<span class="sas">SAS</span>';};
-  if (content[6][i] == 1) {langage = langage + '<span class="python">pyt</span>';};
+  if (content[4][i] == 1) {langage = langage + '<span class="r"></span>';};
+  if (content[5][i] == 1) {langage = langage + '<span class="sas"></span>';};
+  if (content[6][i] == 1) {langage = langage + '<span class="python"></span>';};
 
   if (content[1][i] !== null ) {
     classe = content[1][i];
@@ -239,7 +240,7 @@ for (var i = 0; i <= menuSub.length - 1; i++) {
     } else {
       angle.removeClass('fa-angle-right');
       angle.addClass('fa-angle-down');
-      valeur = 'block';
+      valeur = 'flex';
     };
     var elems = document.getElementsByClassName(menuSub);
 
