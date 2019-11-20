43d26226dff4e7b72df01ffbf5c061cf3cd43e64
gdemare <44768462+gdemare@users.noreply.github.com>
2019-11-20 14:15:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ $(document).ready(function() {
 // dossier ; sous dossier; fichier; general; r; sas; python
 content = [
 ['company',null,'animateur',1,0,0,0],
-['company',null,'MOA-MOE',1,0,0,0],
 ['company',null,'job-interview',1,0,0,0],
-['database',null,'data-warehouse',1,0,0,0],
+['company',null,'MOA-MOE',1,0,0,0],
 ['database',null,'business-object',1,0,0,0],
+['database',null,'data-warehouse',1,0,0,0],
 ['database',null,'json',1,0,0,0],
 ['database',null,'sql',1,0,0,0],
 ['database',null,'talend',1,0,0,0],
