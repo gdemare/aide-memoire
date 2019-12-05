7b9d3dad04c55dd51975781aa6a24893287425a5
gdemare <44768462+gdemare@users.noreply.github.com>
2019-12-05 17:06:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ content = [
 ['statistique',null,'formater-donnees',0,1,1,0],
 ['statistique',null,'graphiques',0,1,1,0],
 ['statistique',null,'manipuler-donnees',0,1,1,0],
-['statistique',null,'programme-indépendant_r.md',0,1,0,0],
+['statistique',null,'programme-indépendant',0,1,0,0],
 ['statistique',null,'reduction-dimensions',1,0,0,0],
 ['statistique',null,'statistique',1,0,0,0],
 ['statistique',null,'statistique-descriptive',0,0,1,0],
